f58ecb405fc7cd66520fa2b8638f10ee2a4c8e9b
jiiinii <seahee422@gmail.com>
2023-12-01 18:16:19 +0900

    Resolving errors that reload the page when clicking on a button. Click a button to opo up the data

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,26 +1,47 @@
-$.ajax({
-    method: "GET",
-    url: "https://dapi.kakao.com/v3/search/book",
-    data: {
-        query: "animal",
-        page: 50,
-        size: 10,
-        target: "",
-        status: ""
-    },
-    headers: { Authorization: "KakaoAK 0c604b6d9932c79e6b756db42c60334b" },
-    // 쿼리 파라미터 갯수 요청하기
+//검색창에 엔터 치면 결과가 나오도록 함
+// searchPost()실행
+window.enterkeySearch = () => {
+    console.log(`enterkeySearch`);
+    if (window.event.keyCode == 13) {
+        searchPost();
+    }
+};
 
-})
-    .done(function (msg) {
-        console.log(msg);
-        const inputGroup = document.querySelector(".input-group");
-        for (var i = 0; i < 10; i++) {
-            const tmp = `
-        <li class = "books">
-        <img class = "book_poster" src="${msg.documents[i].thumbnail}"/><br/>
-        </li>
-        `
-            inputGroup.innerHTML += tmp;
-        } // 2
-    });
+window.searchPost = () => {
+    console.log(`searchPost`);
+    $('#input-group').empty();
+    let searchQuery = $('#search-input').val();
+    console.log(`searchQuery :  ` + searchQuery);
+    if (searchQuery == '') {
+        $('#search-input').focus();
+        return;
+    }
+
+    console.log(`searchPost ajax`);
+    $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v3/search/book",
+        data: {
+            query: "animal",
+            page: 50,
+            size: 10,
+            target: "",
+            status: ""
+        },
+        headers: { Authorization: "KakaoAK 0c604b6d9932c79e6b756db42c60334b" },
+        // 쿼리 파라미터 갯수 요청하기
+
+    })
+        .done(function (msg) {
+            console.log(msg);
+            const inputGroup = document.querySelector(".input-group");
+            for (var i = 0; i < 10; i++) {
+                const tmp = `
+                <li class = "books">
+                <img class = "book_poster" src="${msg.documents[i].thumbnail}"/><br/>
+                </li>
+                `
+                inputGroup.innerHTML += tmp;
+            } // 2
+        });
+}
