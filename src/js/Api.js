934836494da37825b069b60760bb63f7b44fe927
jiiinii <seahee422@gmail.com>
2023-12-11 17:52:34 +0900

    Resolve blank image errors. Derive different page results on screen.

--- src/js/Api.js
+++ src/js/Api.js
@@ -12,36 +12,37 @@ window.searchPost = () => {
     let searchQuery = $('.search-entry').val();
     console.log(`searchQuery :  ` + searchQuery);
 
-    $.ajax({
-        method: "GET",
-        url: `https://dapi.kakao.com/v3/search/book`,
-        data: {
-            query: searchQuery,
-            page: 50,
-            size: 10,
-            target: "",
-            status: ""
-        },
-        headers: { Authorization: "KakaoAK 0c604b6d9932c79e6b756db42c60334b" },
-        // 쿼리 파라미터 갯수 요청하기
+    const inputGroup = document.querySelector(".input-group");
+    inputGroup.innerHTML = "";
 
-    })
-        .done(function (msg) {
-            const inputGroup = document.querySelector(".input-group");
-            let tmp;
-            for (var i = 0; i < 10; i++) {
-                tmp = `${msg.documents[i].thumbnail === ""
-                    ? `<li class = "books"><img class = "book-poster-none"></img></li>`
-                    : `<li class = "books"><img class = "book_poster" src="${msg.documents[i].thumbnail}"/></li>`
-                }`
-                displayResults(inputGroup, tmp);
-                inputGroup.innerHTML += tmp;
-            } // 2
+    const booksEl = document.createElement('ul');
+    booksEl.className = 'booksList';
 
-            function displayResults(inputGroup, tmp) {
-                // 결과를 표시하기 전에 이전 결과 지우기
-                inputGroup.innerHTML = "";
-                tmp.innerHTML = "";
-            }
-        });
+    for (let test = 1; test < 3; test++) {
+        $.ajax({
+            method: "GET",
+            url: `https://dapi.kakao.com/v3/search/book`,
+            data: {
+                query: searchQuery,
+                page: test, // 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
+                size: 50, // 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
+                target: "",
+                status: ""
+            },
+            headers: { Authorization: "KakaoAK 0c604b6d9932c79e6b756db42c60334b" },
+            // 쿼리 파라미터 갯수 요청하기
+
+        })
+            .done((msg) => {
+                console.log(msg);
+                for (var i = 0; i < msg.documents.length; i++) {
+                    tmp = `${msg.documents.length && msg.documents[i].thumbnail === ""
+                        ? `<li class = "books"><img class = "book-poster-none"></img></li>`
+                        : `<li class = "books"><img class = "book_poster" src="${msg.documents[i].thumbnail}"/></li>`
+                        }`
+                    inputGroup.innerHTML += tmp;
+                } // 2                 
+            });
+    }
+    inputGroup.append(booksEl);
 }
