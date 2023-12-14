a893dc7662ed6f535d5ac42e4cf48aab4da03ac9
ParkJinHui <jiinii0104@outlook.com>
2023-12-14 16:04:33 +0900

    Fix the code from for loop to map function

--- src/js/Api.js
+++ src/js/Api.js
@@ -18,31 +18,30 @@ window.searchPost = () => {
     const booksEl = document.createElement('ul');
     booksEl.className = 'booksList';
 
-    for (let test = 1; test < 3; test++) {
-        $.ajax({
-            method: "GET",
-            url: `https://dapi.kakao.com/v3/search/book`,
-            data: {
-                query: searchQuery,
-                page: test, // 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
-                size: 50, // 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
-                target: "",
-                status: ""
-            },
-            headers: { Authorization: "KakaoAK 0c604b6d9932c79e6b756db42c60334b" },
-            // 쿼리 파라미터 갯수 요청하기
+    $.ajax({
+        method: "GET",
+        url: `https://dapi.kakao.com/v3/search/book`,
+        data: {
+            query: searchQuery,
+            page: 1, // 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
+            size: 50, // 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
+            target: "",
+            status: ""
+        },
+        headers: { Authorization: "KakaoAK 0c604b6d9932c79e6b756db42c60334b" },
+        // 쿼리 파라미터 갯수 요청하기
 
-        })
-            .done((msg) => {
-                console.log(msg);
-                for (var i = 0; i < msg.documents.length; i++) {
-                    tmp = `${msg.documents.length && msg.documents[i].thumbnail === ""
-                        ? `<li class = "books"><img class = "book-poster-none"></img></li>`
-                        : `<li class = "books"><img class = "book_poster" src="${msg.documents[i].thumbnail}"/></li>`
-                        }`
-                    inputGroup.innerHTML += tmp;
-                } // 2                 
+    })
+        .done((msg) => {
+            console.log(msg);
+
+            msg.documents.map(result => {
+                let tmp = `${result.thumbnail === ""
+                ? `<li class = "books"><img class = "book-poster-none"></img></li>`
+                : `<li class = "books"><img class = "book_poster" src="${result.thumbnail}"/></li>`
+                }`;
+                booksEl.innerHTML += tmp;
             });
-    }
+        });
     inputGroup.append(booksEl);
 }
