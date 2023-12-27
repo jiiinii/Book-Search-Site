ca3c8c8068ceb735f158b6eba9cd9ad3c0effeb9
ParkJinHui <jiinii0104@outlook.com>
2023-12-27 18:45:53 +0900

    Page button output completed according to a small number of search results(Error exists).

--- src/js/Api.js
+++ src/js/Api.js
@@ -22,16 +22,16 @@ window.searchPost = () => {
   pageButton.style.display = "block"; // 페이지 버튼 출력
 
   let pageNationHTML = "";
-  var pageSize = 1;
-  var countPerPage = 8; // 한 페이지당 n개씩 보여줄 것.
+  var currentPage = 1;
+  var rowsPerPage = 8; // 한 페이지당 n개씩 보여줄 것.
 
   $.ajax({
     method: "GET",
     url: `https://dapi.kakao.com/v3/search/book`,
     data: {
       query: searchQuery,
-      page: pageSize, // 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
-      size: countPerPage, // 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
+      page: currentPage, // 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
+      size: rowsPerPage, // 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
       target: "",
       status: "",
     },
@@ -41,7 +41,6 @@ window.searchPost = () => {
       // return Math.ceil(data.length / sizeVal); // data.length = 총 파싱해 온 데이터 수량
     },
   }).done((msg) => {
-    console.log(msg);
     if (msg.documents.length !== 0) {
       msg.documents.forEach((element) => {
         const booksLiEl = document.createElement("li");
@@ -84,30 +83,115 @@ window.searchPost = () => {
       inputGroup.append(noResults);
       pageButton.style.display = "none";
     }
+
+    const rows = document.querySelectorAll('.product-list li');
+    const rowsCount = msg.meta.total_count; // 총 검색 결과 수 (항목의 총 개수)
+    const pageCount = Math.ceil(rowsCount / rowsPerPage); // 최대 페이지 개수
+    const numbers = document.querySelector('#numbers');
+    const prevPageBtn = document.querySelector('.pagenation .fa-arrow-left');
+    const nextPageBtn = document.querySelector('.pagenation .fa-arrow-right');
+    let pageActiveIdx = 0; // 현재 페이지 그룹의 번호
+    // let currentPage = 0; // 현재 보고있는 페이지네이션 번호
+    let maxPageNum = 10; // 페이지그룹 최대 개수
+
+    console.log("rowsCount : " + rowsCount);
+    console.log("pageCount : " + pageCount);
+
+    for(let a = 1; a <= pageCount; a++) {
+        numbers.innerHTML += `<li class = "page_box"><a href="">${a}</a></li>`;
+    }
+
+    // 페이지네이션 클릭. numbersBtn: 페이지네이션 번호 숨기기
+    const numbersBtn = numbers.querySelectorAll('a');
+
+    //페이지네이션 번호 감추기
+    for(nb of numbersBtn){
+        nb.style.display = 'none';
+    }
+
+    numbersBtn.forEach((item, idx) => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault() // 기본기능 막기
+
+            displayRow(idx) // displayRow : 페이지 번호에 해당하는 항목을 표시, 나머지 항목 숨김
+        })
+    });
+    
+    function displayRow(idx) {
+        let start = idx * rowsPerPage;
+        let end = start * rowsPerPage;
+        let rowsArray = [...rows];
+
+        for(ra of rowsArray) {
+            ra.style.display = '';
+        }
+
+        let newRows = rowsArray.slice(start, end);
+        for(nr of newRows) {
+            nr.style.display = '';
+        }
+
+        for(nb of numbersBtn){
+            nb.classList.remove('active');
+        }
+        numbersBtn[idx].classList.add('active');
+
+        console.log("idx:: " + idx);
+
+
+    } //displayRow
+
+    displayRow(0);
+
+    // 페이지네이션 그룹표시 함수
+    function displayPage(num){
+
+        // 페이지네이션 번호 감추기
+        for(nb of numbersBtn){
+            nb.style.display = 'none';
+        }
+
+        let totalPageCount = Math.ceil(pageCount / maxPageNum);
+        let pageArr = [...numbersBtn];
+        let start = num * maxPageNum;
+        let end = start + maxPageNum;
+        let pageListArr = pageArr.slice(start, end);
+
+
+        for(pa of pageListArr){
+            pa.style.display = 'block';
+        }
+        // 첫 페이지에서 화살표 x
+        if(pageActiveIdx == 0){
+            prevPageBtn.style.display = 'none';
+        } else {
+            prevPageBtn.style.display = 'block';
+        }
+        
+        // 마지막 페이지에서 화살표 x
+        if(pageActiveIdx == totalPageCount-1) {
+            nextPageBtn.style.display = 'none';
+        } else {
+            nextPageBtn.style.display = 'block';
+        }
+    }
+    displayPage(0);
+
+    nextPageBtn.addEventListener('click', () => {
+        let nextPageNum = pageActiveIdx * maxPageNum + maxPageNum;
+        displayRow(nextPageNum);
+        ++pageActiveIdx;
+        displayPage(pageActiveIdx);
+    });
+
+    prevPageBtn.addEventListener('click', () => {
+        let prevPageNum = pageActiveIdx * maxPageNum - maxPageNum;
+        displayRow(prevPageNum);
+        --pageActiveIdx;
+        displayPage(pageActiveIdx);
+    });
+
   });
 
-
-  // 페이지 버튼 클릭 이벤트
-
-  var page_box = document.getElementsByClassName("page_box");
-
-  function handleClick(event) {
-    console.log(this);
-
-    if (event.target.classList[1] === "clicked") { //event.target -> 이벤트가 일어날 객체
-      event.target.classList.add("clicked");
-    } else {
-      for (var i = 0; i < page_box.length; i++) {
-        page_box[i].classList.remove("clicked");
-      }
-      event.target.classList.add("clicked");
-    }
-  }
-  function init() {
-    for (var i = 0; i < page_box.length; i++) {
-      page_box[i].addEventListener("click", handleClick);
-    }
-  }
-  init();
 
 };
