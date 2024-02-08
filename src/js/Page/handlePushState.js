5b8970ad133a55940c65f2ae33c4782bc36370ae
ParkJinHui <jiinii0104@outlook.com>
2024-02-08 14:58:00 +0900

    Fix the code in handlePushState file

--- src/js/Page/handlePushState.js
+++ src/js/Page/handlePushState.js
@@ -1,3 +1,10 @@
 export const handlePushState = (obj, path) => {
-    
+    obj.addEventListener ('click', (e) => {
+        e.prevendDefault();
+        window.history.pushState('', '', path);
+        const urlChange = new CustomEvent({
+            detail: { href: path },
+        });
+        document.dispatchEvent(urlChange);
+    })
 }
